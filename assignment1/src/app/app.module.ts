1d20b307c2e94e7ad996c68f362ce49bda203df4
Alex Schin <Alex.Schin@endava.com>
2019-08-13 15:11:05 +0300

    add succes alert component

--- assignment1/src/app/app.module.ts
+++ assignment1/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
