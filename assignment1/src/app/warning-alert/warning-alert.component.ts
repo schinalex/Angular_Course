5094721e66c03ffed08b7716836717a95688096c
Alex Schin <Alex.Schin@endava.com>
2019-08-13 15:04:39 +0300

    add warning alert component

---
+++ assignment1/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css']
+})
+
+export class WarningAlertComponent implements OnInit {
+  constructor() { }
+
+  message = 'Warning, zombie apocalypse!'
+
+  ngOnInit() {
+  }
+}
