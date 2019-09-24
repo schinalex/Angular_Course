06f33473556be46340105c8d2d490fff69ae6e89
Alex Schin <Alex.Schin@endava.com>
2019-09-24 10:54:12 +0300

    finishing up shopping list... for now

--- project/src/app/shopping-list/shopping-list.component.ts
+++ project/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Ingredient } from '../shared/ingredient.model'
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = []
 
   constructor() { }
 
