4234254f7e627f0fb0a7352fa63627407b1dd503
Alex Schin <Alex.Schin@endava.com>
2019-10-16 17:34:04 +0300

    move code from app component into cockpit and server components

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>()
+  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>()
+
+  newServerName = ''
+  newServerContent = ''
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddServer() {
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
+  }
 }
