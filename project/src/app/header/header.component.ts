34422e8facc476a128025c1a35fd8d44d408842a
Alex Schin <Alex.Schin@endava.com>
2019-10-17 13:43:19 +0300

    add route changed event emitter

--- project/src/app/header/header.component.ts
+++ project/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() routeChnaged = new EventEmitter<string>()
 
   constructor() { }
 
