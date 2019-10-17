f5b08767a7c8cca4dc6d21bcf5d0363f597e4287
Alex Schin <Alex.Schin@endava.com>
2019-10-17 17:13:50 +0300

    show details when a recipe is selected

--- project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<Recipe>()
+
   recipes: Recipe[] = [
     new Recipe('Chopped Salad', 'Quick basic chopped salad', 'https://www.tablefortwoblog.com/wp-content/uploads/2018/05/quick-chopped-salad-recipe-photos-tablefortwoblog-3.jpg')
   ];
